ab7d75741d158bb06eca54ad811acd94ee4422a3
Md Ahadullah <108590177+ahad5333@users.noreply.github.com>
2024-04-24 07:07:09 +0530

    Add files via upload

---
+++ controlFlow.js
@@ -0,0 +1,88 @@
+// const temperature = 41;
+// if(temperature > 50){
+//     console.log("temperature is greater than 50")
+
+// }
+// else{
+//     console.log("temperature is less than 50")
+// }
+
+// ---------------------------------------------------
+// const score = 200;
+// if(score > 100){
+//     const power = "fly";
+//     console.log(`User power: ${power}`)
+// }
+// console.log(`User power: ${power}`)
+
+// have to look into scope here the power variable can only accessible in within the scope it cannot be accessible outside of the scope
+
+// short hand notation of if else
+// const balance = 500;
+// if(balance > 500) console.log("test")
+
+// const balance = 1000;
+// if(balance < 500){
+//     console.log("less than 500");
+// }else if(balance < 750){
+//     console.log("less than 750")
+
+// }
+// else if(balance === 1000){
+
+//     console.log("greater than 1000")
+// }
+
+//switch case
+// const month = 2;
+// switch (month) {
+//   case 1:
+//     console.log("Jan");
+//     break;
+//   case 2:
+//     console.log("FEB");
+//     break;
+//   case 3:
+//     console.log("March");
+//     break;
+//   case 4:
+//     console.log("April");
+//     break;
+//   case 5:
+//     console.log("May");
+//     break;
+//   default:
+//     break;
+// }
+
+// ___________________________________
+
+// falsy value
+
+// false, 0, -0,BigInt 0n, null, undefined, NaN
+
+
+// _____________________________________
+// truthy values
+
+// true, 1, "0","false"," ",[],{},function(){}
+
+// const username = []
+
+// if(username.length === 0){
+//     console.log("Array is empty")
+// }
+
+// const emptyObj = {}
+//here Object.keys convert object into array then we check length of the array with .length that is equal to 0 or not
+// if(Object.keys(emptyObj).length === 0){
+//     console.log("Object is empty")
+
+// }
+
+
+// Terinary operator
+// condition ? true : false
+
+// const iceTeaPrice = 100
+// iceTeaPrice <=80 ? console.log("less than 80") : console.log("greater than 80")
